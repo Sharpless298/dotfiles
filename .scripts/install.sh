0cce650d45d1a838b078c65ee8a1bd6ee7b0f7c9
Sharpless298 <sharpless298@gmail.com>
2024-05-21 20:32:40 +0800

    upd

---
+++ .scripts/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v vim)" ]; then
+  echo '错误：vim 未安装，请先安装 vim.' >&2
+  exit 1
+fi
+
+PLUG_VIM_DIR="${HOME}/.vim/autoload"
+mkdir -p "${PLUG_VIM_DIR}"
+
+echo "Downloading vim-plug..."
+curl -fLo "${PLUG_VIM_DIR}/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if [ $? -ne 0 ]; then
+  echo 'Falied.' >&2
+  exit 1
+fi
+
+echo "vim-plug installed!"
+
