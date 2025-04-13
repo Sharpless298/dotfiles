d6f668ee729c1a899b9eb4ca5d20121e56093bde
Sharpless298 <sharpless298@gmail.com>
2025-04-14 02:18:04 +0800

    add autocmd for c cpp tex

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,4 +6,58 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c" },
+	callback = function()
+		vim.schedule(function()
+			-- vim.keymap.set("n", "<F9>", ":w <bar> !gcc % -lm -Wall -Wextra -Wconversion -Wshadow<CR>")
+			-- vim.keymap.set("n", "<F10>", ":term ./a.out<CR>")
+
+			vim.keymap.set("n", "<F9>", function()
+				vim.cmd("w | term gcc % -Wall -Wextra -Wconversion -Wshadow -fsanitize=address,undefined")
+				vim.cmd("startinsert")
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				vim.cmd("term ./a.out;")
+				vim.cmd("startinsert")
+			end)
+		end)
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "cpp" },
+	callback = function()
+		vim.schedule(function()
+			-- vim.keymap.set(
+			-- 	"n",
+			-- 	"<F9>",
+			-- 	":w <bar> !g++ -std=c++23 % -Wall -Wextra -Wconversion -Wshadow -fsanitize=address,undefined<CR>"
+			-- )
+			-- vim.keymap.set("n", "<F10>", ":term ./a.out<CR>")
+			--
+			vim.keymap.set("n", "<F9>", function()
+				vim.cmd("w | term g++ -std=c++23 % -Wall -Wextra -Wconversion -Wshadow -fsanitize=address,undefined")
+				vim.cmd("startinsert")
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				vim.cmd("term ./a.out;")
+				vim.cmd("startinsert")
+			end)
+		end)
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "tex" },
+	callback = function()
+		vim.schedule(function()
+			vim.keymap.set("n", "<F9>", function()
+				vim.cmd("w | term pdflatex %")
+				vim.cmd("startinsert")
+			end)
+			vim.keymap.set("n", "<F10>", ":!zathura %:r.pdf &<CR>")
+		end)
+	end,
+})
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,3 +6,4 @@ vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.autochdir = true
